2e73b577aefbaa9cd8f3290b86373cee96a23625
oguz-can <oguz-can.oezcetin@codenorm.de>
2023-07-25 13:46:52 +0200

    - smtp.js versucht zu integrieren um durch Kontaktformular E-Mail zu versenden - Meta Title für SEO optimiert. - Kleine Änderungen an den Seiten vorgenommen (designtechnisch)

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import '../../assets/smtp.js';
+declare let Email: any;
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  constructor() { }
+  onSubmit() {
+    const message = {
+      Host: "smtp.elasticemail.com",
+      Username: "kontakt@dreamofgraphy.de",
+      Password: "83337F352597163F60A6A0D14E0A45460BA3",
+      To: 'kontakt@dreamofgraphy.de',
+      From: (document.getElementById("email") as HTMLInputElement).value,
+      Subject: "Neuer Kontakt von DreamOfGraphy's Kontaktformular",
+      Body: "Vorname: " + (document.getElementById("firstName") as HTMLInputElement).value
+        + "<br> Nachname " + (document.getElementById("lastName") as HTMLInputElement).value
+        + "<br> Email " + (document.getElementById("email") as HTMLInputElement).value
+        + "<br> Telefonnummer " + (document.getElementById("phone") as HTMLInputElement).value
+        + "<br> Firma " + (document.getElementById("company") as HTMLInputElement).value
+        + "<br> Wie sind sie auf uns aufmerksam geworden? " + (document.getElementById("attention") as HTMLSelectElement).value
+        + "<br> Betreff " + (document.getElementById("subject") as HTMLSelectElement).value
+        + "<br> Nachricht " + (document.getElementById("message") as HTMLTextAreaElement).value
+    };
 
-  ngOnInit(): void {
+    Email.send(message).then(
+      (response: any) => {
+        alert("Nachricht wurde erfolgreich gesendet!");
+        // Formular zurücksetzen
+        const form = document.querySelector('.contact-form') as HTMLFormElement;
+        form.reset();
+      },
+      (error: any) => {
+        alert("Beim Senden der Nachricht ist ein Fehler aufgetreten!");
+      }
+    );
   }
-
 }
